468064a84c35b2e9f6d162621568d2c49366abef
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-12-22 21:23:49 +0100

    Skeleton for svg

--- noeuds/svg.adb
+++ noeuds/svg.adb
@@ -1,73 +1,80 @@
-with Ada.Text_IO, Ada.Integer_Text_IO;
-use Ada.Text_IO, Ada.Integer_Text_IO;
+with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Float_Text_IO;
+use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Float_Text_IO;
+
+with Objets;
+use Objets;
 
 package body Svg is
    
-   --  type Color is (Violet, Indigo, Bleu, Vert, Jaune, Orange, Rouge, Noir, Blanc);
-   
-   --  Display_Width, Display_Height : Natural;
-   --  Fichier_Svg : File_Type;
+   type Color is (Violet, Indigo, Bleu, Vert, Jaune, Orange, Rouge, Noir, Blanc);  
 
-   --  function Code_Couleur (C : Color) return String is
-   --  begin
-   --     case C is
-   --        when Violet  => return "rgb(255,0,255)";
-   --        when Indigo  => return "rgb(111,0,255)";
-   --        when Bleu  => return "rgb(0,0,255)";
-   --        when Vert  => return "rgb(0,255,0)";
-   --        When Jaune => return "rgb(0,255,255)";
-   --        When Orange => return "rgb(255,165,0)";
-   --        when Rouge => return "rgb(255,0,0)";
-   --        when Noir  => return "rgb(0,0,0)";
-   --        when Blanc => return "rgb(255,255,255)";
-   --     end case;
-   --  end Code_Couleur;
-
-   --  -- ! A appeler avant toute ecriture dans le fichier svg !
-   --  -- garantit : Insere le header svg dans Fichier_Svg.
-   --  procedure Svg_Header (Largeur, Hauteur : Natural) is
-   --  begin
-   --     Put (Fichier_Svg, "<svg width=""");
-   --     Put (Fichier_Svg, Largeur);
-   --     Put (Fichier_Svg, """ height=""");
-   --     Put (Fichier_Svg, Hauteur);
-   --     Put_Line (Fichier_Svg, """>");
-
-   --     Display_Width := Largeur;
-   --     Display_Height := Hauteur;
-   --  end Svg_Header;
-
-   --  -- ! A appeler pour clore le fichier svg !
-   --  -- garantit : Insere le footer svg dans Fichier_Svg.
-   --  procedure Svg_Footer is
-   --  begin
-   --     Put_Line (Fichier_Svg, "</svg>");
-   --  end Svg_Footer;
-
-   --  -- Dessine une ligne A -- B
-   --  procedure Svg_Line (A, B : Point)
-   --  is
-   --  begin
-   --     Put (Fichier_Svg, "<line x1=""");
-   --     Put (Fichier_Svg, A.X);
-   --     Put (Fichier_Svg, """ y1=""");
-   --     Put (Fichier_Svg, A.Y);
-   --     Put (Fichier_Svg, """ x2=""");
-   --     Put (Fichier_Svg, B.X);
-   --     Put (Fichier_Svg, """ y2=""");
-   --     Put (Fichier_Svg, B.Y);
-   --     Put (Fichier_Svg, """ style=""stroke:");
-
-   --     Put (Fichier_Svg, Code_Couleur (Noir));
-
-   --     Put_Line (Fichier_Svg, ";stroke-width:1""/>");
-   --  end Svg_Line;
+   function Code_Couleur (C : Color) return String is
+   begin
+      case C is
+         when Violet  => return "rgb(255,0,255)";
+         when Indigo  => return "rgb(111,0,255)";
+         when Bleu  => return "rgb(0,0,255)";
+         when Vert  => return "rgb(0,255,0)";
+         When Jaune => return "rgb(0,255,255)";
+         When Orange => return "rgb(255,165,0)";
+         when Rouge => return "rgb(255,0,0)";
+         when Noir  => return "rgb(0,0,0)";
+         when Blanc => return "rgb(255,255,255)";
+      end case;
+   end Code_Couleur;
    
    procedure Sauvegarde (Nom_Fichier_Svg : in String;
                          L : in Liste_Sommets)
    is
+      Fichier_Svg : File_Type;
+      
+      -- ! A appeler avant toute ecriture dans le fichier svg !
+      -- garantit : Insere le header svg dans le fichiersvg.
+      procedure Svg_Header is
+      begin
+	 Put (Fichier_Svg, "<svg width=""");
+	 Put (Fichier_Svg, Affichage.Largeur);
+	 Put (Fichier_Svg, """ height=""");
+	 Put (Fichier_Svg, Affichage.Hauteur);
+	 Put_Line (Fichier_Svg, """>");
+      end Svg_Header;
+
+      -- ! A appeler pour clore le fichier svg !
+      -- garantit : Insere le footer svg dans le fichier svg.
+      procedure Svg_Footer is
+      begin
+	 Put_Line (Fichier_Svg, "</svg>");
+      end Svg_Footer;
+      
+      -- Dessine une ligne A -- B
+      procedure Svg_Line (A, B : Point)
+      is
+      begin
+	 Put (Fichier_Svg, "<line x1=""");
+	 Put (Fichier_Svg, A.X);
+	 Put (Fichier_Svg, """ y1=""");
+	 Put (Fichier_Svg, A.Y);
+	 Put (Fichier_Svg, """ x2=""");
+	 Put (Fichier_Svg, B.X);
+	 Put (Fichier_Svg, """ y2=""");
+	 Put (Fichier_Svg, B.Y);
+	 Put (Fichier_Svg, """ style=""stroke:");
+
+	 Put (Fichier_Svg, Code_Couleur (Noir));
+
+	 Put_Line (Fichier_Svg, ";stroke-width:1""/>");
+      end Svg_Line;
+      
    begin
-      null;
+      Create (File => Fichier_Svg,
+	    Mode => Out_File,
+	    Name => Nom_Fichier_Svg);
+      
+      Svg_Header;      
+      
+      Svg_Footer;      
+      
+      Close (Fichier_Svg);            
    end Sauvegarde;
    
 end Svg;
--- noeuds/svg.ads
+++ noeuds/svg.ads
@@ -3,6 +3,14 @@ use Liste;
 
 package Svg is
    
+   type Image is record
+      Largeur : Float;
+      Hauteur : Float;
+   end record;
+   
+   Affichage : Image;
+   Marge_Affichage : constant Float := 10.0;
+   
    procedure Sauvegarde (Nom_Fichier_Svg : in String;
                          L : in Liste_Sommets);
 end Svg;
