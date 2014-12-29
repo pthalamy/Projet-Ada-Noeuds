8cba0232b7494c4e2f17de7392ab1e2008db167d
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-12-29 13:13:01 +0100

    Termine l'implementation de la pile
    Ajoute une fonction pour tester si la pile est vide.

--- noeuds/Pile.ads
+++ noeuds/Pile.ads
@@ -2,28 +2,22 @@ with Objets;
 use Objets;
 
 package Pile is
-   
-   type Cellule;
-   type Pile is access Cellule;
-   
-   type Cellule is record
-      Val : Indice;
-      Suiv : Pile;
-   end record;
-   
+      
    Pile_Vide, Memoire_Pleine : exception;   
    
-   procedure Push (Tete : in out Pile; I : in Indice);
+   procedure Push (Tete : in out Pile_Sommets; I : in Indice);
    
    -- Ne devrait pas avoir à être utilisé, mais si le fichier
    -- kn est mal formaté (indices sommets voisins non triés),
    -- permet d'ajouter un element en fin de pile.
-   -- MAIS: Important coût en tempspotentiel, responsabilité 
+   -- MAIS: Important coût en temps potentiel, responsabilité 
    -- de l'utilisateur que  de bien formatter son fichier de donnée.
-   --  procedure Push_Back (Tete : in out Pile; I : in Indice)
+   --  procedure Push_Back (Tete : in out Pile_Sommets; I : in Indice)
    
-   procedure Pop (Tete : in out Pile;
+   procedure Pop (Tete : in out Pile_Sommets; I : out Indice);
    
-   procedure Put (Tete : in Pile);
+   function Vide (Tete : in Pile_Sommets) return Boolean;
+   
+   procedure Put (Tete : in Pile_Sommets);
    
 end Pile;
--- noeuds/pile.adb
+++ noeuds/pile.adb
@@ -1,14 +1,14 @@
-with Ada.Text_IO, Ada.Unchecked_Deallocation;
-use Ada.Text_IO;
+with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Unchecked_Deallocation;
+use Ada.Text_IO, Ada.Integer_Text_IO;
 
 with Objets;
 use Objets;
 
 package body Pile is
    
-   procedure Push (Tete : in out Pile; I : in Indice) is
+   procedure Push (Tete : in out Pile_Sommets; I : in Indice) is
    begin                 
-      Tete := new Pile'(I, Tete);
+      Tete := new Cellule'(I, Tete);
       
       if Tete = null then
 	 raise Memoire_Pleine;	 
@@ -16,8 +16,8 @@ package body Pile is
       
    end Push;
    
-   --  procedure Push_Back (Tete : in out Pile; I : in Indice) is
-   --     Cour : Pile := Tete;
+   --  procedure Push_Back (Tete : in out Pile_Sommets; I : in Indice) is
+   --     Cour : Pile_Sommets := Tete;
    --  begin                 
    --     while Cour /= null loop
    --  	 Cour := Cour.Suiv;
@@ -26,24 +26,25 @@ package body Pile is
    --     Push (Cour, I)            
    --  end Push;
    
-   procedure Pop (Tete : in out Pile) is
-      procedure Free is new Ada.Unchecked_Deallocation(Cellule, Pile);
-      Temp : Tete;
+   procedure Pop (Tete : in out Pile_Sommets; I : out Indice) is
+      procedure Free is new Ada.Unchecked_Deallocation(Cellule, Pile_Sommets);
+      Temp : Pile_Sommets := Tete;
    begin
       if Temp = null then
 	 raise Pile_Vide;
       end if;
       
+      I := Tete.Val;
       Tete := Tete.Suiv;
       Free (Temp);
       
    exception
-      when => Pile_Vide
+      when Pile_Vide =>
 	 Put_Line ("Pile_Vide: Impossible de dépiler, la pile est vide.");
    end Pop;
    
-   procedure Put (Tete : in Pile) is 
-      Cour : Pile;
+   procedure Put (Tete : in Pile_Sommets) is 
+      Cour : Pile_Sommets;
    begin
       if Tete = null then
 	 raise Pile_Vide;
@@ -53,7 +54,7 @@ package body Pile is
       
       while Cour /= null loop
 	 New_Line;
-	 Put (Cour.Val);
+	 Put (Natural (Cour.Val));
 	 Cour := Cour.Suiv;
 	 New_Line;
       end loop;
@@ -62,5 +63,14 @@ package body Pile is
       when Pile_Vide =>
 	 Put_Line ("Pile_Vide: Affichage impossible, la pile est vide.");	 
    end Put;    
+   
+   function Vide (Tete : in Pile_Sommets) return Boolean is 
+   begin 
+      if Tete = null then
+	 return True;
+      else 
+	 return False;
+      end if;
+   end Vide;
    
 end Pile;
