f88aae9bdfb30ac36d5f0b4849907aaaaabca1ac
Benji118 <benjamin.lebit@yahoo.fr>
2015-01-20 13:30:53 +0100

    Please enter the commit message for your changes.Decomm de put et quelques commentaires sur les procedures de calcul(pdc et angle)

--- noeuds/svg.adb
+++ noeuds/svg.adb
@@ -97,7 +97,7 @@ package body Svg is
 	    while Local_Cour /= null loop
 	       if Local_Cour.Ind = SCour then
 		  ArreteCible := Local_Cour.A.all;
-		  Put_Line("indice" & Integer'Image( Integer (Local_Cour.Ind)));
+		  --  Put_Line("indice" & Integer'Image( Integer (Local_Cour.Ind)));
 		  Put (ArreteCible);
 		  return; 
 	       end if;
@@ -110,12 +110,12 @@ package body Svg is
 	 is
 	    
 	    Angle : Float;
-	    LScour_Sopp :Float;
-	    LSOpp_Scand:Float;
-	    LScand_Scour:Float;
+	    LScour_Sopp :Float; 	--  Longueur de l'arrete courante
+	    LSOpp_Scand:Float;         	--  Distance entre le sommet opposé de l'arrete courante et le sommet candidat
+	    LScand_Scour:Float;		--  Longueur de l'arrete candidate
 	    Base : constant Float := 360.0;
 	    Interm : Float;
-	    Determinant:Float;
+	    Determinant:Float;		--  Determinant des vecteurs (Scour_Sopp,Scour_Scand)
 	    
 	    function Signe (Expression : Float) return Float is
 	    begin
@@ -128,9 +128,9 @@ package body Svg is
 	    
 	 begin
 	    
-	    Put (Scour); 
-	    Put (SOpp); 
-	    Put (SCand);
+	    --  Put (Scour); 
+	    --  Put (SOpp); 
+	    --  Put (SCand);
 	    
 	    LScour_Sopp := sqrt((Sopp.X-Scour.X)**2 + (Sopp.Y-Scour.Y)**2);
 	    
@@ -144,6 +144,7 @@ package body Svg is
 	    Interm := ((Sopp.X-Scour.X)*(SCand.X-Scour.X) +
 	 		 (Sopp.Y-Scour.Y)*(SCand.Y-Scour.Y)) /
 	      (LScour_Sopp*LScand_Scour);	    	    	    
+	    
 	    
 	    Angle := Arccos(Interm , Base)*Signe(Determinant);
 	    
@@ -168,9 +169,9 @@ package body Svg is
 	    SCand := Cour.Ind;
 	    
 	    if SCand /= SOpp then
-	       Put_Line ("SCour="&Integer'Image(Integer(SCour))
-			   &"  SOpp="&Integer'Image(Integer(SOpp))
-			   &"  SCand="&Integer'Image(Integer(SCand)));
+	       --  Put_Line ("SCour="&Integer'Image(Integer(SCour))
+	       --  		   &"  SOpp="&Integer'Image(Integer(SOpp))
+	       --  		   &"  SCand="&Integer'Image(Integer(SCand)));
 	       
 	       AngleCour := Calcul_Angle (T(SCour).Pos, 
 	       				  T(SOpp).Pos, 
@@ -178,19 +179,18 @@ package body Svg is
 					  Trig);
 	       	       	       
 	       if AngleCour < AngleMin then
-		  Put_Line ("AngleCour: " & Float'Image (AngleCour)
-			      & " < AngleMin: " & Float'Image (AngleMin));
+		  --  Put_Line ("AngleCour: " & Float'Image (AngleCour)
+		  --  	      & " < AngleMin: " & Float'Image (AngleMin));
 		  AngleMin := AngleCour;
 		  SCible := SCand;
-	       else
-		  Put_Line ("AngleCour: " & Float'Image (AngleCour) & " > AngleMin: " & Float'Image (AngleMin));
+	       --  else
+		  --  Put_Line ("AngleCour: " & Float'Image (AngleCour) & " > AngleMin: " & Float'Image (AngleMin));
 	       end if;
 	    end if;
 	    
 	    Cour := Cour.Suiv;
 	 end loop;
-	 
-	 Put_Line ("SCible="&Integer'Image(Integer(SCible)));
+	 	 --  Put_Line ("SCible="&Integer'Image(Integer(SCible)));
 	 Recupere_ArreteCible; 
 	 
       exception
@@ -213,16 +213,16 @@ package body Svg is
 	    Path:=True;
 	 end if;
 	 if Trig then
-	    Put_Line("Milieu Départ :" & "(" & Float'Image(ArreteCour.Milieu.X) & " ' " & Float'Image(ArreteCour.Milieu.Y) & ")");
-	    Put_Line("Milieu Arrivé :" & "(" & Float'Image(ArreteCible.Milieu.X) & " ' " & Float'Image(ArreteCible.Milieu.Y) & ")");
-	    Put_Line("Points de Controles :" & "(Trig Cour)" & Point2Str (ArreteCour.MyPDC.Trig) & "(Inv Cible)" & Point2Str (ArreteCible.OppPDC.Inv));
+	    --  Put_Line("Milieu Départ :" & "(" & Float'Image(ArreteCour.Milieu.X) & " ' " & Float'Image(ArreteCour.Milieu.Y) & ")");
+	    --  Put_Line("Milieu Arrivé :" & "(" & Float'Image(ArreteCible.Milieu.X) & " ' " & Float'Image(ArreteCible.Milieu.Y) & ")");
+	    --  Put_Line("Points de Controles :" & "(Trig Cour)" & Point2Str (ArreteCour.MyPDC.Trig) & "(Inv Cible)" & Point2Str (ArreteCible.OppPDC.Inv));
 	    Svg_Curve (ArreteCour.Milieu, ArreteCible.Milieu, 
 		       ArreteCour.MyPDC.Trig, ArreteCible.OppPDC.Inv);
 	    Trig := False;
 	 else
-	    Put_Line("Milieu Départ :" & "(" & Float'Image(ArreteCour.Milieu.X) & " ' " & Float'Image(ArreteCour.Milieu.Y) & ")");
-	    Put_Line("Milieu Arrivé :" & "(" & Float'Image(ArreteCible.Milieu.X) & " ' " & Float'Image(ArreteCible.Milieu.Y) & ")");
-	    Put_Line("Points de Controles :" & "(Inv Cour)" & Point2Str (ArreteCour.MyPDC.Inv) & "(Trig Cible)" & Point2Str (ArreteCible.OppPDC.Trig));
+	    --  Put_Line("Milieu Départ :" & "(" & Float'Image(ArreteCour.Milieu.X) & " ' " & Float'Image(ArreteCour.Milieu.Y) & ")");
+	    --  Put_Line("Milieu Arrivé :" & "(" & Float'Image(ArreteCible.Milieu.X) & " ' " & Float'Image(ArreteCible.Milieu.Y) & ")");
+	    --  Put_Line("Points de Controles :" & "(Inv Cour)" & Point2Str (ArreteCour.MyPDC.Inv) & "(Trig Cible)" & Point2Str (ArreteCible.OppPDC.Trig));
 	    Svg_Curve (ArreteCour.Milieu, ArreteCible.Milieu, 
 		       ArreteCour.MyPDC.Inv, ArreteCible.OppPDC.Trig);	 
 	    Trig := True;
@@ -234,7 +234,7 @@ package body Svg is
    begin    
       -- Initialisation
       Depart := T'First; -- On part toujours du premier element
-      Put_Line ("Départ: " & Integer'Image (Integer(Depart)));
+      --  Put_Line ("Départ: " & Integer'Image (Integer(Depart)));
       Put (T(Depart));
       SCour := Depart;      
       ArreteCour := T(SCour).Voisins.Tete.A.all;
@@ -249,7 +249,7 @@ package body Svg is
 	 Trouve_Cible;
 	 Trace_Bezier;
 	 
-	 Put_Line ("SCible: " & Integer'Image (Integer(SCible)));
+	 --  Put_Line ("SCible: " & Integer'Image (Integer(SCible)));
 	 
 	 SCour := SCible;
 	 ArreteCour := ArreteCible;
